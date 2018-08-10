781fa070444349c0df0198f637beec53186e8d6b
Gazi Yamin Iqbal <gaziyaminiqbal@gmail.com>
2018-08-10 17:29:43 +0600

    added to testing

--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	"net/http"
 	"encoding/json"
 	"log"
+	"strconv"
+	"fmt"
 
 	"github.com/gorilla/mux"
 )
 
 type shoppingItem struct {
 	Id int	`json:"Id, omitempty"`
-	Name string `json:"Name"`
+	Name string `json:"Name, omitempty"`
 	Price float64 `json:"Price, omitempty"`
+	Count int `json:"Id, omitempty"`
 }
 
 type Response struct {
@@ -22,7 +25,7 @@ type Response struct {
 
 //var shoppingItemList = make(map[shoppingItem]bool)
 var shoppingItemList []shoppingItem
-var itemCount int
+var idCount int
 
 func getShoppingList (w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Response{Success: 1, Message: "The Shopping List", ShoppingItem: shoppingItemList})
@@ -30,17 +33,17 @@ func getShoppingList (w http.ResponseWriter, r *http.Request) {
 
 func addShoppingItem (w http.ResponseWriter, r *http.Request) {
 	var shoppingItemInput shoppingItem
-	
+
 	err := json.NewDecoder(r.Body).Decode(&shoppingItemInput)
 
 	if err == nil {
-		if shoppingItemInput.Name == "" {
+		if shoppingItemInput.Name == "" || shoppingItemInput.Price < 0 || shoppingItemInput.Count < 0  {
 			w.WriteHeader(http.StatusBadRequest)
 			json.NewEncoder(w).Encode(Response{Success: 0, Message: "Invalid or insufficient information"})
 			return
 		}
-		itemCount++
-		shoppingItemInput.Id = itemCount
+		idCount++
+		shoppingItemInput.Id = idCount
 		shoppingItemList = append(shoppingItemList, shoppingItemInput)
 		w.WriteHeader(http.StatusCreated)
 		json.NewEncoder(w).Encode(Response{Success: 1, Message: "Item added successfully"})
@@ -50,19 +53,50 @@ func addShoppingItem (w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func updateShoppingItem (w http.ResponseWriter, r *http.Request) {
+	var shoppingItemInput shoppingItem
+	tmpVar := mux.Vars(r)
+	reqIdx, _ := strconv.Atoi(tmpVar["id"])
+	
+	json.NewDecoder(r.Body).Decode(&shoppingItemInput)
+
+	if shoppingItemInput.Name == "" || shoppingItemInput.Price < 0 || shoppingItemInput.Count < 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Response{Success: 0, Message: "Invalid information"})
+		return
+	}
+
+	for i, item := range shoppingItemList {
+			if item.Id == reqIdx {
+				fmt.Println(shoppingItemInput.Id)
+				shoppingItemList[i] = shoppingItemInput
+				return
+			}
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(Response{Success :0, Message: "Invalid item ID or item not found"})
+}
+
+func deleteShoppingItem (w http.Response, r *http.Request) {
+
+}
+
+
 func main() {
 //	shoppingItemList[shoppingItem{1, "Kolagach", 34.5}]
 //	shoppingItemList[0] = shoppingItem{1, "kolagach", 34.5};
-	itemCount = 0
-	shoppingItemList = append(shoppingItemList, shoppingItem{1, "Kolagach", 34.5})
+	idCount = 0
+	fmt.Println("DOING")
+	shoppingItemList = append(shoppingItemList, shoppingItem{1, "Kolagach", 34.5, 1})
 
 	m := mux.NewRouter()
 
 	m.HandleFunc("/shoppinglist/list",  getShoppingList).Methods("GET")
 
-	m.HandleFunc("/shoppingList/list", addShoppingItem).Methods("POST")
-//	m.HandleFunc("/shoppingList/list", updateShoppingItem).Methods("PUT")
-//	m.HandleFunc("/shoppingList/list/{id}", deleteShoppingItem).Methods("DELETE")
+	m.HandleFunc("/shoppinglist/list", addShoppingItem).Methods("POST")
+	m.HandleFunc("/shoppinglist/list/{id}", updateShoppingItem).Methods("PUT")
+//	m.HandleFunc("/shoppinglist/list/{id}", deleteShoppingItem).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", m))
 }
